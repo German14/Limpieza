50f05a20fa55204ecd786bc782c5ee9b0d3c8c29
german <ggaristo@codelab17.com>
2019-09-11 15:49:51 +0200

    update export to xls

--- Limpieza/src/app/service/service.ts
+++ Limpieza/src/app/service/service.ts
@@ -62,7 +62,7 @@ public href;
 
   public exportAsExcelFile(json: any[], excelFileName: string): void {
     const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
-    const workbook: XLSX.WorkBook = { Sheets: { worksheet }, SheetNames: ['data'] };
+    const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
     const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
     this.saveAsExcelFile(excelBuffer, excelFileName);
   }
