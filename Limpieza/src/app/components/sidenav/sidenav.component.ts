5652a18b222073a72fc5b5515d9c6ab4c8817097
German <germangaristo1989@gmail.com>
2019-09-21 19:09:55 +0200

    selected sidenav

--- Limpieza/src/app/components/sidenav/sidenav.component.ts
+++ Limpieza/src/app/components/sidenav/sidenav.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit, ChangeDetectorRef, OnDestroy} from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
 import {MediaMatcher} from '@angular/cdk/layout';
 import {AuthenticationService} from "../../_service/AuthentificationService";
 import * as jwt_decode from 'jwt-decode';
+
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
@@ -14,14 +15,16 @@ export class SidenavComponent implements OnInit,OnDestroy {
     id:'0',
     name:'Trabajadoras',
     url:'table',
-    enable:true
+    enable:true,
+    icon:'build'
   },
     {
-    id:'1',
-    name:'Clientes',
-    url:'client',
-    enable:false
-  }];
+      id:'1',
+      name:'Clientes',
+      url:'client',
+      enable:false,
+      icon: 'assignment_ind'
+    }];
   private mobileQueryListener: () => void;
 
   constructor(media: MediaMatcher, changeDetectorRef: ChangeDetectorRef, private authorization: AuthenticationService,) {
@@ -31,6 +34,15 @@ export class SidenavComponent implements OnInit,OnDestroy {
   }
   ngOnDestroy(): void {
     this.mobileQuery.removeListener(this.mobileQueryListener);
+  }
+  lastItem;
+  selected(item){
+    if(this.lastItem!==item.name){
+      this.arrayItems[1].enable =!this.arrayItems[1].enable;
+      this.arrayItems[0].enable =!this.arrayItems[0].enable;
+      this.lastItem=item.name;
+    }
+
   }
 
   ngOnInit() {
